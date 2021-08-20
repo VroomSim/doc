b306bf095cf5b5720a58a4b7ee74c521729bada2
VroomSim <89253291+VroomSim@users.noreply.github.com>
2021-08-20 14:23:14 +0300

    Update install.sh

--- nespi/install.sh
+++ nespi/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SourcePath=https://raw.githubusercontent.com/RetroFlag/retroflag-picase/master
+SourcePath=https://raw.githubusercontent.com/VroomSim/doc/main/nespi
 
 #Check if root--------------------------------------
 if [[ $EUID -ne 0 ]]; then
